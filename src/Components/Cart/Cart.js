549b79c223198b60693299c85a84ad93ab26d51a
amitmahlawat <amitmahlawat3@gmail.com>
2022-12-11 12:12:03 +0530

    add_remove button functioning

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,18 +2,40 @@ import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 import React, { useContext } from "react";
 import CartCnxt from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
   const Cartcntxt = useContext(CartCnxt);
+
+  const cartItemAddHandler = (item) => {
+    Cartcntxt.addItem(item)
+    
+  };
+
+  const cartItemRemoveHandler = (item) => {
+   Cartcntxt.removeItem(item)
+  };
+
+  
   const CartItems = (
     <ul className={classes["cart-items"]}>
       {Cartcntxt.items.map((item) => (
-        <li>
-          Name: {item.name} Price:{item.price} quantity:{item.quantity}
-        </li>
+      item.quantity>0 &&  <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          quantity={item.quantity}
+          onRemove={cartItemRemoveHandler.bind(null, item)}
+          onAdd={cartItemAddHandler.bind(null, {...item, quantity: 1})}
+        />
       ))}
     </ul>
   );
+  const hasItems = Cartcntxt.items.length > 0;
+
+  
+
   let totalAmount = 0;
   Cartcntxt.items.forEach((item) => {
     totalAmount = totalAmount + item.price * item.quantity;
@@ -30,7 +52,7 @@ const Cart = (props) => {
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,17 +1,51 @@
-import React,{ useState } from 'react';
+import React,{ useEffect, useState } from 'react';
 import CartContext from './cart-context';
 
 
 
 const CartProvider = (props) => {
+    
 const[items,updateItems]=useState([])
 
   const addItemToCartHandler = (item) => {
-    updateItems([...items,item])
-    console.log(items)
-  };
+    let ExistingItem=items.findIndex((data)=>{
+        return data.id===item.id;
 
-  const removeItemFromCartHandler = (id) => {
+    })
+    
+    
+    if(ExistingItem===-1){
+        updateItems([...items,item])
+        
+        
+    }else{
+        const temp=[...items]
+        temp[ExistingItem].quantity=Number(temp[ExistingItem].quantity) + Number(item.quantity)
+        updateItems(temp)
+    }}
+
+   
+
+  const removeItemFromCartHandler = (item) => {
+    // // console.log('working')
+    
+    // let ExistingItem=items.findIndex((data)=>{
+    //     return (data.id)
+        
+    // })
+    
+    // console.log(ExistingItem)
+    // if(ExistingItem=0){
+        const temp=[...items]
+        if(item.quantity>=1){
+        item.quantity=item.quantity - 1}
+        updateItems(temp)
+    //     // items[ExistingItem].price=Number(items[ExistingItem].price) - Number(items[ExistingItem].price)
+        
+    //  }
+    // console.log()
+
+    
     
   };
 
@@ -21,6 +55,8 @@ const[items,updateItems]=useState([])
     removeItem: removeItemFromCartHandler,
     
   };
+
+  
 
   return (
     <CartContext.Provider value={cartContext}>
